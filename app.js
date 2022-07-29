a38c6ad61ef1d0e20846f1f81d6f649894ec06c3
kbkim303 <kbkim303@gmail.com>
2022-07-29 15:09:16 +0900

    그림그리는 판 만들기, 선의 굵기 조절 바 추가

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const colors = [
@@ -8,18 +9,37 @@ const colors = [
   "#bdc3c7",
   "#f1c40f",
 ];
+
 canvas.width = 800;
 canvas.height = 800;
 
-canvas.addEventListener("click", onClick);
+let isPainting = false;
 
-function onClick(event) {
-  ctx.beginPath();
-  ctx.moveTo(400, 400);
-  const color = colors[Math.floor(Math.random() * colors.length)];
-  ctx.strokeStyle = color;
-  ctx.lineTo(event.offsetX, event.offsetY);
-  ctx.stroke();
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
 }
 
-canvas.addEventListener("mousemove", onClick);
+function onMouseDown(event) {
+  isPainting = true;
+}
+
+function onMouseUp(event) {
+  isPainting = false;
+  ctx.beginPath();
+}
+
+function onLineWidthChange(event) {
+  ctx.beginPath();
+  ctx.lineWidth = event.target.value;
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", onMouseDown);
+canvas.addEventListener("mouseup", onMouseUp);
+canvas.addEventListener("mouseleave", onMouseUp);
+lineWidth.addEventListener("change", onLineWidthChange);
