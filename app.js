6e6b5d6a13a02fb61be9a1b0f56474be09836720
kbkim303 <kbkim303@gmail.com>
2022-07-29 16:30:43 +0900

    사진넣는 기능 만듦

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const colorOptions = Array.from(
   document.getElementsByClassName("color-option")
 );
+const fileInput = document.getElementById("file");
 const modeBtn = document.getElementById("mode-btn");
 const eraserAllBtn = document.getElementById("eraser-all-btn");
 const eraserModeBtn = document.getElementById("eraser-btn");
@@ -8,6 +9,7 @@ const lineWidth = document.getElementById("line-width");
 const lineColor = document.getElementById("line-color");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+
 const CANVAS_WIDTH = 800;
 const CANVAS_HEIGHT = 800;
 
@@ -81,6 +83,16 @@ function onEraserClick(event) {
   ctx.lineWidth = event.target.value;
 }
 
+function onFileChange(event) {
+  const file = event.target.files[0];
+  const url = URL.createObjectURL(file);
+  const image = new Image();
+  image.src = url;
+  image.onload = function () {
+    ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  };
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", onMouseDown);
 canvas.addEventListener("mouseup", onMouseUp);
@@ -94,3 +106,4 @@ colorOptions.forEach((color) =>
 modeBtn.addEventListener("click", onModeClick);
 eraserAllBtn.addEventListener("click", onEraserAllClick);
 eraserModeBtn.addEventListener("click", onEraserClick);
+fileInput.addEventListener("change", onFileChange);
